053d89d11b05ca4dc36754a90283788cb9167f6f
Olga <oolgaa75@mail.ru>
2023-02-18 19:59:10 +0300

    Task One

---
+++ test/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <LessonOne />
+
+</template>
+
+<script>
+
+import LessonOne from './components/LessonOne.vue';
+
+export default {
+  name: 'App',
+  components: {
+    LessonOne,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ test/src/components/global/MainButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <button
+        type="button"
+        class="main-button"
+      >
+        <span class="main-button__label">
+          <slot></slot>
+        </span>
+    </button>
+</template>
+
+<script>
+
+
+export default {
+  name: 'MainButton',
+}
+</script>
+
+<styles scoped lang="scss">
+  @import './styles/MainButton/component';
+</styles>
